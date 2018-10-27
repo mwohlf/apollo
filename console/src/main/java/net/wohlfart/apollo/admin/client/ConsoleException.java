36a75464cfe75c7981a98bab33b74a7b632c1a08
michael <michael@wohlfart.net>
2018-10-27 17:33:44 +0200

    stable gradle build

--- console/src/main/java/net/wohlfart/apollo/admin/client/ConsoleException.java
+++
@@ -1,9 +0,0 @@
-package net.wohlfart.apollo.admin.client;
-
-public class ConsoleException extends RuntimeException {
-
-    public ConsoleException(String message) {
-        super(message);
-    }
-
-}
