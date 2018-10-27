2f82d7364624149763e1668abdde18bb35a57509
michael <michael@wohlfart.net>
2018-10-27 20:17:14 +0200

    index routing

--- backend/src/main/java/net/wohlfart/apollo/controller/IndexController.java
+++ backend/src/main/java/net/wohlfart/apollo/controller/IndexController.java
@@ -1,8 +1,16 @@
 package net.wohlfart.apollo.controller;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import org.springframework.web.reactive.result.view.Rendering;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
 
+import java.net.URI;
 
 
 // @Api
@@ -12,10 +20,16 @@ public class IndexController {
 
     /*
      * angular single page subpages mapping
+     * this is a redirect but should be a proxy to the index.html
      */
-    @GetMapping(path = {"/", "/index", "/login", "/label", "/settings", "/admin", "/home" })
-    public String index() {
-        return "forward:index.html";
+    @GetMapping(path = {"", "/", "/page1", "/page2", "/page3"})
+    public Mono<Void> index(ServerWebExchange exchange) {
+      ServerHttpResponse response = exchange.getResponse();
+      response.setStatusCode(HttpStatus.PRECONDITION_FAILED);
+      response.getHeaders().add(HttpHeaders.LOCATION, "/index.html");
+      return response.setComplete();
+      // Rendering.redirectTo("abc").build()
+      //  return ServerResponse.temporaryRedirect(URI.create("index.html")).build();
     }
 
 }
